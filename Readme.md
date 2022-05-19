3d136d23b64469720f4e1d5146edba6cf7e0936e
Tadeusz Gozdek <Tadeusz.Gozdek@bury.com>
2022-05-19 11:58:35 +0200

    Add project files.

---
+++ Readme.md
@@ -0,0 +1,12 @@
+Simple FTDI UM201XB replacement.
+
+Works also with the LGT8fxxx boards.
+Checked with Arduino Nano ATMega168 (1Mbit UART) & LGT8F328P (460kbit UART)
+
+Connections:
+- SCL (A5)
+- SDA (A4)
+- GND
+
+Project uses twi.c - TWI/I2C library for Wiring & Arduino
+Copyright (c) 2006 Nicholas Zambetti
---
+++ nano_twi_console.ino
@@ -0,0 +1,75 @@
+
+extern "C" {
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include "twi.h"
+}
+
+// TWI/I2C addres taken over from FTDI UM201XB
+const uint8_t TWI_address = 0x22;
+const uint32_t TWI_speed = 400000;
+
+#ifdef ARDUINO_AVR_LARDU_328E
+// LGT8F328 460800 boud @ 32MHz,
+const uint32_t UART_speed = 460800;
+#elif defined(ARDUINO_AVR_NANO)
+// ATMega168 UART 1000000 boud
+const uint32_t UART_speed = 1000000;
+#else
+// For unknow CPU just 115200 boud
+const uint32_t UART_speed = 115200;
+#endif
+
+bool dataPresent = false;
+
+/*
+   Client receive data callback
+*/
+void onReceiveService(uint8_t* inBytes, int numBytes)
+{
+  //Get bytes from buffer and send to UART
+  while (numBytes > 0) {
+    char c = *inBytes++;
+    Serial.print(c);
+    numBytes--;
+  }
+
+  // Trigger LED
+  dataPresent = true;
+}
+
+/*
+   Setup HW (LED, UART, TWI/I2C)
+*/
+void setup()
+{
+  // Prepare LED GPIO
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Prepare UART
+  Serial.begin(UART_speed);
+  Serial.println("NanoTWI console @ 0x22");
+
+  // Set TWI speed, initialize, register callback, set slave address
+  twi_setFrequency(TWI_speed);
+  twi_init();
+  twi_attachSlaveRxEvent(onReceiveService);
+  twi_setAddress(TWI_address);
+}
+
+/*
+   Main loop
+*/
+void loop()
+{
+  if (dataPresent) {
+    // Blink LED when I2C is receiving data
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(50);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(50);
+
+    dataPresent = 0;
+  }
+}
