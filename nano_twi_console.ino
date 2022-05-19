11b208e3099b23cf00f5a6660cf2c3096bd071f4
Tadeusz Gozdek <Tadeusz.Gozdek@bury.com>
2022-05-19 12:52:44 +0200

    Add license disclaimer (as it is free software).

--- nano_twi_console.ino
+++ nano_twi_console.ino
@@ -1,3 +1,11 @@
+/*
+  Copyright (c) 2022 fotonix@pm.me  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+ */
 
 extern "C" {
 #include <stdlib.h>
